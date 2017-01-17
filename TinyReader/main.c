385d769810849838d3cbfb16e628da8853326520
PhoenixPL512 <phoenixpl512@gmail.com>
2017-01-17 11:32:06 +0000

    seriously, nothing is working

--- TinyReader/main.c
+++ TinyReader/main.c
@@ -1,6 +1,15 @@
-#include "usiTwiSlave.h"
+//#include "usiTwiSlave.h"
+#include <avr/io.h>
+#include <util/delay.h>
+
 // main code
 int main() {
-  usiTwiSlaveInit(0x69);
+  // usiTwiSlaveInit(0x69);
+  DDRB = 3;
+  PORTB = 3;
+  while (1) {
+    _delay_ms(500);
+    PORTB ^= 3;
+  }
   return 0;
 }
