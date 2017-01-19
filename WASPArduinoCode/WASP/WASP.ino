7c3b3fce7f0b16e1a9c51b4165401bba2236b270
PhoenixPL512 <phoenixpl512@gmail.com>
2017-01-19 12:27:14 +0100

    bump

--- WASPArduinoCode/WASP/WASP.ino
+++ WASPArduinoCode/WASP/WASP.ino
@@ -13,16 +13,22 @@
 #include <Arduino.h>
 #include <SD.h>
 #include <SPI.h>
+#include <Wire.h>
 
 // DEFINES
+/* I2C scanner found 3 devices: 0x1E, 0x5D and 0x6B */
+#define __DEBUG__ 1
 #define SD_CS_PIN A1
+#define LPS331_I2C_ADDRESS 0x5D
+#define LIS3MDL_I2C_ADDRESS 0x1E
+#define LSM6_I2C_ADDRESS 0x6B
 
 // GLOBAL VARIABLES
 File logFile;
 
 // FUNCTIONS
 // # SD Card management #
-inline void logInit() {
+inline void initLog() {
   SD.begin(SD_CS_PIN);
   logFile = SD.open("LOG.TXT", FILE_WRITE);
   logFile.println("\n\n[LOGFILE OPENED]");
@@ -45,7 +51,32 @@ template <typename T> inline void logWriteReading(String name, T value) {
   logFile.flush();
 }
 
+// # Misc I2C functions used by sensors #
+inline void writeReg(byte address, byte reg, byte value) {
+#if __DEBUG__
+  logFile.print(millis());
+  logFile.print("[DEBUG] Writing value ");
+  logFile.print(value);
+  logFile.print("to reg ");
+  logFile.print(reg);
+  logFile.print(" to device at ");
+  logFile.println(address);
+#endif
+
+  Wire.beginTransmission(address);
+  Wire.write(reg);
+  Wire.write(value);
+  Wire.endTransmission();
+}
+
+// # Sensors management #
+inline void initSensors() {
+  // init LPS331 (temperature/pressure/attitude sensor)
+  Wire.begin(LPS331_I2C_ADDRESS);
+  writeReg(LPS331_I2C_ADDRESS, 0x20, 0b11100000);
+}
+
 // SETUP/LOOP
-void setup() { logInit(); }
+void setup() { initLog(); }
 
 void loop() {}
